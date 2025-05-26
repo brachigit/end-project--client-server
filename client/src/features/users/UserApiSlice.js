7b9c6178c970fe0bc597e0f466244d501b929f84
Chedva02 <chedva03030@gmail.com>
2025-05-26 21:41:22 +0300

    Get user with skip

--- client/src/features/users/UserApiSlice.js
+++ client/src/features/users/UserApiSlice.js
@@ -2,14 +2,28 @@ import apiSlice  from "../../App/apiSlice";
 
 const UserApiSlice=apiSlice.injectEndpoints({
     endpoints:(build)=>({
-        getUsers:build.query({
+        /*getUsers:build.query({
             query:()=>({
                 url:"/api/user",
                 
             }),providesTags:["users"]
+        }),*/
+        getUsers:build.query({
+            query:({skip,limit})=>({
+                url:`/api/user/skip?skip=${skip}&limit=${limit}`,
+                
+            }),providesTags:["getUsers"]
+        }),
+        addNewManager:build.mutation({
+            query:(user)=>({
+                url:'/api/user/manager',
+                method:"POST",
+                body: user
+                
+            }),invalidatesTags:["getUsers"]
         }),
        
     })
 })
 
-export const { useGetUsersQuery}=UserApiSlice
+export const { useGetUsersQuery,useAddNewManagerMutation}=UserApiSlice
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -5,9 +5,10 @@ const verifyJWT=require("../middleware/verifyJWT")
 const managerJWT=require("../middleware/managerJWT")
 
 router.get("/",verifyJWT,managerJWT,users.getAllUser)
+router.get("/skip",verifyJWT,managerJWT,users.getUserBySkip)
 router.get("/:id",verifyJWT,users.getUserById)
 router.post("/",users.creatNewUser)
-router.post("/",verifyJWT,managerJWT,users.creatNewManager)
+router.post("/manager",verifyJWT,managerJWT,users.creatNewManager)
 router.delete("/:id",verifyJWT,users.deleteUser)
 router.put("/:id",verifyJWT,users.updateUser)
 
